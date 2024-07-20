0248445da6465d2a7e54dfb94a3e2b25c213d2a6
Ilgiz Saburov <barysaburov@gmail.com>
2024-07-20 14:50:54 +0300

    add new file style.js

---
+++ style.js
@@ -0,0 +1,15 @@
+const currencySale = +prompt('Укажите курс продажи валюты'); 
+        const currencyBuy = +prompt('Укажите курс покупки валюты');
+        const quantityMoney = +prompt('Сколько денег вы хотите обменять ?'); 
+        const exchangeMoney = +prompt('Сколько раз вы хотите обменять деньги ?');
+        
+        const exchangeResult = (currencyBuy / currencySale) ** exchangeMoney;
+        const userResult = Math.round(exchangeResult * quantityMoney);
+
+        const exchangeProfit = quantityMoney - userResult;
+
+        alert('У вас осталось ' + userResult + ' Коммисия за обмен ' + exchangeProfit); 
+    
+        if (currencySale <= currencyBuy || quantityMoney <= currencyBuy) {
+            alert('Проверьте правильность ввода курса или недостачно средств для обмена!');
+        }
